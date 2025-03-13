949b8b010bed7898500a9ea335399bb2248e56d7
miles <zengyuan9101@gmail.com>
2025-03-13 13:02:31 +0800

    init bank transaction manage project

---
+++ src/main/java/com/github/miles/transaction/BankTransactionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.miles.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransactionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTransactionDemoApplication.class, args);
+	}
+
+}
